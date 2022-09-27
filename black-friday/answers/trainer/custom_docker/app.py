d5f38abf5881b2e536eddb0fc2dbb19575d80093
richard truong-chau <truongc2@illinois.edu>
2022-09-27 15:37:36 -0500

    Add code for custom prediciton

---
+++ black-friday/answers/trainer/custom_docker/app.py
@@ -0,0 +1,37 @@
+import joblib
+import numpy as np
+import pickle
+from flask import Flask, jsonify,request
+
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route('/predict',methods=['POST', "GET"])
+def predict():
+    data = request.get_json(force=True)["instances"]
+    print("############", data)
+    # input_data = request.json.get('instances')
+    
+    # print(input_data)
+    
+
+    #predict
+    prediction = model.predict([data]).tolist()[0]
+    print("-------prediction", prediction)
+
+    #postprocessing
+    output = {'predictions':[
+        prediction
+    ]
+        }
+    return jsonify(output)
+
+@app.route('/healthz')
+def healthz():
+    return "OK"
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
